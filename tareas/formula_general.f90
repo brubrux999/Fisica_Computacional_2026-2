164390bc07253779e86c915409cade18b5a31f07
Bruno Alarcon <brunylarrez@gmail.com>
2026-02-15 18:00:57 -0600

    Eliminar cálculo de raíces con precisión cuádruple

--- tareas/formula_general.f90
+++ tareas/formula_general.f90
@@ -1,6 +1,5 @@
 complex :: a1,b1,c1,x1,x2
 complex(8) :: a2,b2,c2,y1,y2
-complex(16) :: a3,b3,c3,z1,z2
 
 ! Cálculo de las raices con precisión simple
 a1 = (1., 1.)
@@ -33,21 +32,5 @@ print*, x2, "----> x2"
 ! Comprobación de resultados
 print*, a2*y1**2 + b2*y1 + c2, "----> Comprobación de x1"
 print*, a2*y2**2 + b2*y2 + c2, "----> Comprobación de x2"
-
-! Cálculo de las raices con precisión doble
-a3 = (1.d0, 1.d0)
-b3 = (2.d0, 2.d0)
-c3 = (3.d0, 3.d0)
-
-z1 = (-b3 + SQRT(b3**2 - 4*a3*c3))/(2*a3)
-z2 = (-b3 -SQRT(b3**2 - 4*a3*c3))/(2*a3)
-
-print*, "Precisión cuádruple:"
-print*, x1, "----> x1"
-print*, x2, "----> x2"
-
-! Comprobación de resultados
-print*, a3*z1**2 + b3*z1 + c3, "----> Comprobación de x1"
-print*, a3*z2**2 + b3*z2 + c3, "----> Comprobación de x2"
 
 end program
